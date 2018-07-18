d0634d07e4ddf01243ee619d7ac70c3d0ce43b2f
李蔚生 <971353677@qq.com>
2018-07-18 19:11:02 +0800

    feat: 创建完模板文件后执行 up to date in 1.07s 命令安装依赖

--- libs/generate.js
+++ libs/generate.js
@@ -1,51 +1,78 @@
 const fs = require('fs')
+const ora = require('ora')
 const path = require('path')
 const glob = require('glob')
 const chalk = require('chalk')
 const fse = require('fs-extra')
 const ignore = require('ignore')
+const { spawnSync } = require('child_process')
 
 const cwd = process.cwd()
 
 module.exports = (templateName, dir) => {
   let destDir = cwd
+  const spinner = ora()
 
-  if (dir) {
-    destDir = path.resolve(cwd, dir)
+  // 创建模板文件
+  try {
+    spinner.text = '正在创建模板文件'
+    spinner.start()
 
-    if (fs.existsSync(destDir)) {
-      console.log(chalk.red(
-        `当前目录下已经存在目录 ${dir}，` +
-        `为了避免覆盖原来的文件，` +
-        `请重新指定目标目录或删除 ${dir} 目录`
-      ))
-      process.exit(1)
+    if (dir) {
+      destDir = path.resolve(cwd, dir)
+
+      if (fs.existsSync(destDir)) {
+        spinner.stop()
+        console.log(chalk.red(
+          `当前目录下已经存在目录 ${dir}，` +
+          `为了避免覆盖原来的文件，` +
+          `请重新指定目标目录或删除 ${dir} 目录`
+        ))
+        process.exit(1)
+      }
+
+      fse.ensureDirSync(destDir)
     }
 
-    fse.ensureDirSync(destDir)
+    const templateDir = path.resolve(__dirname, '../templates', `./${templateName}`)
+    const gitignoreFilePath = path.resolve(templateDir, './gitignore')
+    const gitignoreFileContent = fs.readFileSync(gitignoreFilePath).toString()
+
+    let templateFilenames = glob.sync('**/*', {
+      cwd: templateDir,
+      dot: true,
+      ignore: ['node_modules/**'],
+    })
+    templateFilenames = ignore().add(gitignoreFileContent).filter(templateFilenames)
+
+    templateFilenames.forEach(filename => {
+      const src = path.resolve(templateDir, filename)
+      const dest = path.resolve(destDir, filename)
+      fse.copySync(src, dest)
+    })
+
+    fs.renameSync(
+      path.resolve(destDir, 'gitignore'),
+      path.resolve(destDir, '.gitignore')
+    )
+
+    spinner.succeed('创建模板文件成功')
+  } catch (error) {
+    spinner.fail('创建模板文件失败')
+    throw error
   }
 
-  const templateDir = path.resolve(__dirname, '../templates', `./${templateName}`)
-  const gitignoreFilePath = path.resolve(templateDir, './gitignore')
-  const gitignoreFileContent = fs.readFileSync(gitignoreFilePath).toString()
+  // 安装依赖
+  try {
+    spinner.text = '正在安装依赖'
+    spinner.start()
+    spawnSync('npm', ['install'], { cwd: destDir })
+    spinner.succeed('安装依赖成功')
+  } catch (error) {
+    spinner.fail('安装依赖失败')
+    throw error
+  }
 
-  let templateFilenames = glob.sync('**/*', {
-    cwd: templateDir,
-    dot: true,
-    ignore: ['node_modules/**'],
-  })
-  templateFilenames = ignore().add(gitignoreFileContent).filter(templateFilenames)
-
-  templateFilenames.forEach(filename => {
-    const src = path.resolve(templateDir, filename)
-    const dest = path.resolve(destDir, filename)
-    fse.copySync(src, dest)
-  })
-
-  fs.renameSync(
-    path.resolve(destDir, 'gitignore'),
-    path.resolve(destDir, '.gitignore')
-  )
-
+  console.log(' ')
   console.log(chalk.green('创建成功！'))
 }
--- libs/index.js
+++ libs/index.js
@@ -1,10 +1,14 @@
+const fs = require('fs')
+const path = require('path')
 const inquirer = require('inquirer')
 const generate = require('./generate')
+
+console.log('快速生成项目开发模板（单页应用、服务器渲染）\n')
 
 inquirer.prompt([{
   name: 'templateName',
   type: 'list',
-  message: '选择模板类型：',
+  message: '请选择需要生成的模板类型：',
   choices: [
     { name: '单页应用（SPA）', value: 'spa' },
     { name: '服务器端渲染（SSR）', value: 'ssr' },
@@ -12,18 +16,20 @@ inquirer.prompt([{
 }, {
   name: 'generateInCurrentDir',
   type: 'list',
-  message: '是否在当前文件夹下生成模板文件',
+  message: '是否在当前文件夹下生成模板文件：',
   default: false,
   choices: [
-    { name: '否，新建文件夹', value: false },
+    { name: '否（新建文件夹）', value: false },
     { name: '是', value: true },
   ],
 }, {
   name: 'dir',
   type: 'input',
-  message: '请输入新的文件夹名称：',
+  message: '请输入新文件夹的名称：',
   validate: value => {
-    if (!value) return '文件夹名称不能为空！'
+    const cwd = process.cwd()
+    if (!value) return '文件夹名称不能为空，请重新输入。'
+    if (fs.existsSync(path.resolve(cwd, value))) return '此文件夹已经存在，请重新输入。'
     return true
   },
   filter: value => {
@@ -31,5 +37,6 @@ inquirer.prompt([{
   },
   when: answers => !answers.generateInCurrentDir,
 }]).then(answers => {
+  console.log(' ')
   generate(answers.templateName, answers.dir)
 })
