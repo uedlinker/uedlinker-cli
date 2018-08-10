b3155aae36425369e33d53076da8140228dfd963
李蔚生 <971353677@qq.com>
2018-08-10 13:08:15 +0800

    feat: 使用 @uedlinker/scripts 作为 SPA 的脚本命令
    1. 更新依赖包

--- templates/spa/src/index.js
+++ templates/spa/src/index.js
@@ -1,7 +1,26 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import registerServiceWorker from 'libs/registerServiceWorker'
+import { render } from 'react-dom'
+
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+const renderApp = Component => {
+  render(
+    <Component />,
+    document.getElementById('root')
+  )
+}
+
+renderApp(App)
+
+// 开启 HMR
+if (module.hot && process.env.NODE_ENV === 'development') {
+  module.hot.accept(['./App'], () => {
+    const App = require('./App').default
+    renderApp(App)
+  })
+}
+
+// 开启离线缓存
+if (process.env.NODE_ENV === 'production') {
+  require('offline-plugin/runtime').install()
+}
