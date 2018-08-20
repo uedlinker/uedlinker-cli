ac721b3b49676f4b44f9a06fac27a57f22c9e0f2
RunningCoderLee <sprint_l@aliyun.com>
2018-08-20 19:56:30 +0800

    feat(templates/console): 添加 react-router

--- templates/console/src/App.jsx
+++ templates/console/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { Router, Route, Switch } from 'react-router-dom'
 
-import store from 'store'
-import Main from 'pages/Main'
+import store from './store'
+import Main from './pages/Main'
+import history from './utils/history'
+
 
 const App = () => (
   <Provider store={store}>
-    <Main />
+    <Router history={history} >
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route render={() => <div>404</div>} />
+      </Switch>
+    </Router>
   </Provider>
 )
 
